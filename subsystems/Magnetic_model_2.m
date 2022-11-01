0508fd8ea21c94b20e2485e59519f0a29f57bdea
Nicolo' Galletta <93077558+Nicolo-Galletta@users.noreply.github.com>
2022-11-01 22:53:58 +0100

    Update Magnetic_model_2.m
    Aggiunto un ulteriore if dentro al ciclo: vedi linee 46-50.
    Aggiunti commentini vari.
    Modello validato sul paper di Virgi e funzionano sia P che dP fino al 12° ordine. Il 13° non è stato testato perchè non mi andava di runnare la WMM di nuovo

--- subsystems/Magnetic_model_2.m
+++ subsystems/Magnetic_model_2.m
@@ -1,15 +1,26 @@
 clearvars; close all; clc;
 
-N = 13; % ordine 
+load("WMM.mat")
+
+N = 12; % ordine (N max is 12 because of K limit in WMM.mat)
 
 n = 1:13;
 m = 0:13;
 
-theta = deg2rad(30); % to test the code, it's an input in the simulink function
-
+lat = 30; % [deg] to test the code, it's an input in the simulink function
+theta = deg2rad(90-lat); % theta is the colatitude [rad]
 
 %% P-dP
-% uguale a fcn su matlab ma non a paper
+% validated through the P and dP tables in "Jeremy Davis, Mathematical Modeling of Earthas Magnetic
+% Field, 2004"
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% P_{(n),(m+1)}: 
+% given P(a,b) -> n=a, m=b-1
+% n from 1 to N
+% m from 0 to N
+% N order of accuracy
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 P = zeros(N,N+1);
 dP = zeros(N,N+1);
@@ -17,20 +28,30 @@ dP = zeros(N,N+1);
 P_00 = 1;
 dP_00 = 0;
 
-P(1,1) = cos(theta);
-dP(1,1) = -sin(theta)*P_00;
+P_01 = 0; %?? ho messo questo valore perchè è quello che sta nelle tabelle del pdf ma ha senso?
+dP_01 = 0;
 
-P(1,2) = sin(theta);  
+P(1,1) = cos(theta); % n=1, m=0
+dP(1,1) = -sin(theta)*P_00; 
+
+P(1,2) = sin(theta);  % n=1, m=1
 dP(1,2) = cos(theta)*P_00;
 
-for i = 2:13
-    for j = 0:i
+for i = 2:N % ciclo in n a partire da n=2 a n=N
+    for j = 0:i % ciclo in m da m=0 a m=n
         if j == i
         P(i,j+1) = sin(theta)*P(i-1,j);
         dP(i,j+1) = sin(theta)*dP(i-1,j) + cos(theta)*P(i-1,j);
         elseif i < 3
-            P(i,j+1) = cos(theta)*P(i-1,j+1);
-            dP(i,j+1) = -sin(theta)*P(i-1,j+1) + cos(theta)*dP(i-1,j+1);
+            if j == 0 % n=2, m=0
+            P(i,j+1) = cos(theta)*P(i-1,j+1) - K(i,j+1)*P_00;
+            dP(i,j+1) = -sin(theta)*P(i-1,j+1) + cos(theta)*dP(i-1,j+1) ...
+                - K(i,j+1)*dP_00;
+            elseif j == 1 % n=2, m=1
+                P(i,j+1) = cos(theta)*P(i-1,j+1) - K(i,j+1)*P_01;
+                dP(i,j+1) = -sin(theta)*P(i-1,j+1) + cos(theta)*dP(i-1,j+1)...
+                    - K(i,j+1)*dP_01;
+            end
         else
             P(i,j+1) = cos(theta)*P(i-1,j+1) - K(i,j+1)*P(i-2,j+1);
             dP(i,j+1) = -sin(theta)*P(i-1,j+1) + cos(theta)*dP(i-1,j+1) ...
@@ -39,3 +60,8 @@ for i = 2:13
     end
 end
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% TO DO:
+% Implementare K in modo da raggiungere ordini superiori a N=12
+% Implementare il codice in modo più carino includendo P_01 e dP_01 nell'if (FACOLTATIVO)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
