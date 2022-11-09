972a6e0122b8e014f32a1f10eba8b87f08d83691
Nicolo' Galletta <93077558+Nicolo-Galletta@users.noreply.github.com>
2022-11-09 17:43:42 +0100

    Added Kinematics 312

--- main.m
+++ main.m
@@ -21,11 +21,11 @@ settings.OM = deg2rad(315.709234758653);    % RAAN [rad]
 settings.om = deg2rad(101.432935891144);    % pericenter's anomaly [rad]
 settings.theta = 0;                         % true anomaly [rad] (assumed equal to zero)
 
-settings.mu = astroConstants(13);            % Earth's planetary constant [km^3/s^2]
+settings.mu = astroConstants(13);           % Earth's planetary constant [km^3/s^2]
 
 [r0, v0] = kep2car(settings);    % initial position and velocity in inertial frame [km]
-settings.w0 = [];                % initial angular velocity in body frame [rad/s]
-settings.E0 = [];                % initial euler angles [rad]
+settings.w0 = [0 0 0]';                % initial angular velocity in body frame [rad/s]
+settings.E0 = [0 0 0]';                % initial euler angles [rad]
 
 settings.N = 13;                 % order of the magnetic field model [-]
 % environment parameters
@@ -47,9 +47,7 @@ environment.WMM.h = h;                          % [nT]
 % satellite parameters
 sat.m  = 720;                                   % Spacecraft mass [kg]
 sat.dipole = 3.5e-3*sat.m * ones(3,1);          % Spacecraft dipole moment [Am^2]
-sat.Ix = [];
-sat.Iy = [];
-sat.Iz = [];
+sat.I = [1 1 1]';                               % Column vector with Principal Inertia Moments
 
 % sensor parameters
 sensor.boh = [];
