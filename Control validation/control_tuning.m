dea801dc6b2f582f98c3a136927c39ca9f6af21b
Giuseppe Brentino <brentinogiuseppe@gmail.com>
2022-12-12 15:37:11 +0100

    test nadir pointing

--- Control validation/control_tuning.m
+++ Control validation/control_tuning.m
@@ -14,13 +14,6 @@ w0 = [1.5e-3 1.5e-3 1.5e-3]';                % initial angular velocity in body 
 % initial attitude
 A_BN0 = [v0'/norm(v0); (cross(r0, v0))'/norm(cross(r0, v0)); r0'/norm(r0)];
 
-q4 = 0.5*sqrt(1 + A_BN0(1, 1) + A_BN0(2, 2) + A_BN0(3, 3));
-q1 = 1/(4*q4) * (A_BN0(2, 3) - A_BN0(3, 2));
-q2 = 1/(4*q4) * (A_BN0(3, 1) - A_BN0(1, 3));
-q3 = 1/(4*q4) * (A_BN0(1, 2) - A_BN0(2, 1));
-
-settings.q0 = [q1 q2 q3 q4]';              % initial estimamted quaternion [-]
-
 n = sqrt(settings.mu/settings.a^3);
 A = [                    0            0   ( sat.I(2)-sat.I(3) )/sat.I(1)*n;...
                          0            0                       0;...
@@ -30,5 +23,14 @@ C = eye(3);
 B = diag([1/sat.I(1) 1/sat.I(2) 1/sat.I(3)]);
 D = zeros(3);
 
-Kp = [0 0 0]';
-Kd = [0 0 0]';
+% lb = - 1*ones(6,1);
+% ub =   1*ones(6,1);
+% options = optimoptions("ga","PlotFcn","gaplotbestf");
+% X = ga(@(X)fitnessfcn(X,n,A,B,C,D,settings,r0,v0,A_BN0,w0),6,[],[],[],[],lb,ub,[],options);
+
+% % load("Nadir.mat")
+% % Kp = 7*X(1:3);
+% % Kd = 2*X(4:6);
+
+Kp = -[-0.1 -0.1 0]';
+Kd = -[10 10 0];
