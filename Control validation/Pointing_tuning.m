012896707a844f412258d1526fcff2e1c67ae6bf
Giuseppe Brentino <brentinogiuseppe@gmail.com>
2022-12-19 23:43:56 +0100

    Test of new control strategy

--- Control validation/Pointing_tuning.m
+++ Control validation/Pointing_tuning.m
@@ -9,19 +9,25 @@ addpath('../functions/');
 configs;
 
 %% Nadir pointing
-w0 = [1e-2 1e-2 1e-2]';                % initial angular velocity in body frame [rad/s]
+settings.w0 = [1e-2 1e-2 1e-2]';                % initial angular velocity in body frame [rad/s]
 
 % initial attitude
 A_BN0 = [v0'/norm(v0); (cross(r0, v0))'/norm(cross(r0, v0)); r0'/norm(r0)];
 n = sqrt(settings.mu/settings.a^3);
-% [E1,E2,E3] = dcm2angle(A_BN0);
-% A0 = angle2dcm(E1+0.7,E2+0.4,E3-1);
-A0 = A_BN0;
-A = [                    0            0   ( sat.I(2)-sat.I(3) )/sat.I(1)*n;...
-                         0            0                       0;...
-     ( sat.I(1)-sat.I(2) )/sat.I(3)*n 0                       0 ];
+[E1,E2,E3] = dcm2angle(A_BN0);
+A0 = angle2dcm(E1+0.7,E2+0.4,E3-1);
+% A0 = A_BN0;
 
-C = eye(3);
-B = diag([1/sat.I(1) 1/sat.I(2) 1/sat.I(3)]);
-D = zeros(3);
+K_w = 1e-2;
+K_cz = sat.I/sat.I(3)*1e-4;%[1 1 1];
+K_cx = sat.I/sat.I(3)*1e-4;%[1 1 1];
 
+settings.E0 = [E1 E2 E3]';                % initial euler angles [rad]
+
+%% GA
+ub = 1000*ones(7,1);
+lb = zeros(7,1);
+
+options = optimoptions('ga','PlotFcn','gaplotbestf','UseParallel', true, 'UseVectorized', false);
+X = ga(@(X)fitnessfcn(X,settings,sat,environment,n,r0,v0,A0,A_BN0),7,[],[],[],[],lb,ub,[],[],options);
+save Pointing_gains X
--- Control validation/fitnessfcn.m
+++ Control validation/fitnessfcn.m
@@ -1,13 +1,9 @@
-function J = fitnessfcn(X, n,A,B,C,D,settings,r0,v0,A_BN0,w0)
+function J = fitnessfcn(X,settings,sat,environment,n,r0,v0,A0,A_BN0)
 
 
-Kp = X(1:3);
-Kd = X(4:6);
-test = sim("PE_test.slx",'SrcWorkspace','Current');
-w = reshape(test.w', [1, size(test.w, 1)*size(test.w, 2)]);
-w_ref = zeros(1, length(w));
-w_ref(2:3:end) = n;
-e = reshape(test.e', [1, size(test.e, 1)*size(test.e, 2)]);
-u = reshape(test.u', [1, size(test.u, 1)*size(test.u, 2)]);
+K_cx = X(1:3);
+K_cz = X(4:6);
+K_w = X(7);
+test = sim("POINTING_2.slx",'SrcWorkspace','Current');
 
-J = 1/2  * (w-w_ref)*(w-w_ref)' + 1/2 * e*e' * 1/2 * u*u' ;
+J = trapz(test.ew + test.theta_x + test.theta_z);
