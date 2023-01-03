707becd06c5792697888b3f5bce1e5bf218f9ea8
rob-pis-nas <nascone.r@icloud.com>
2023-01-03 10:38:39 +0100

    adjustment in A LN

--- functions/configs.m
+++ functions/configs.m
@@ -4,7 +4,7 @@ rng('default');
 plot_style;
 
 Simtime = 50000;            % Maximum simulation time [s]
-% initial attitude and position conditions (Ephemeris 00:00:00 27-10-2022)
+% initial attitude and position conditions
 
 settings.a = 7099.67959313555;              % major semi-axis [km] 
 settings.e = 0.00117256328941186;           % eccentricity [-]
@@ -24,10 +24,8 @@ settings.N = 13;                 % order of the magnetic field model [-]
 
 %% environment parameters
 environment.E_w =  deg2rad(15.04/3600);         % Earth's angular velocity around it's axis [rad/s]
-environment.theta_G0 = 0;                       % Initial Greenwich Meridian Longitude [rad] (mezzanotte 21Settembre)
-environment.theta_E0 = 0;                       % Initial Earth longitude wrt the sun [rad]  (DA CAMBIARE)
+environment.theta_G0 = 0;                       % Initial Greenwich Meridian Longitude [rad] 
 environment.S_w = 2*pi/(365.25*24*3600);        % Earth's angular velocity around the sun [rad/s]
-environment.date = 221*24*3600;                 % Seconds from spring equinox (March 20) to Oct 27 [s]
 environment.Rs = astroConstants(2);             % Sun-Earth distance (1 AU) [km]   
 environment.eps = deg2rad(23.45);               % Inclination of the ecliptic plane [rad]
 environment.R = 6371.2;                         % Earth's radius [km]
